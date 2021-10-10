f6d2188110078844d75f144d382ff7e10c8d75a9
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2021-10-10 17:15:10 +0800

    A

--- resources/views/page/beasiswa/show.blade.php
+++ resources/views/page/beasiswa/show.blade.php
@@ -3,7 +3,7 @@
         <div style="margin-top: 90px">
             <h1 class="text-info">Info Beasiswa Buaat kamu</h1>
             <hr>
-            <img src="{{ asset('img/beaBI.png') }}" alt="">
+            <img src="{{ asset('img/beaBI.jpg') }}" alt="">
             <p class="mt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ullam fuga deserunt inventore, soluta pariatur harum libero eius at, facilis debitis labore explicabo nobis unde doloremque laudantium ex neque consequatur. Id fugiat repellendus minima, quaerat doloremque hic dolore sed in doloribus adipisci eius corporis incidunt ipsum ipsam magni consectetur, distinctio qui, illum animi dolorem odio. Cum tempore quidem adipisci fugiat consequuntur dolorum labore? Unde, voluptates minus, qui esse ullam, ipsa accusamus saepe impedit quisquam nam eius corrupti rerum ex. Cumque, ipsa culpa ducimus, totam quas eaque inventore doloremque dolor atque nostrum rerum et amet saepe. Autem commodi temporibus distinctio vitae.</p>
         </div>
     </div>
