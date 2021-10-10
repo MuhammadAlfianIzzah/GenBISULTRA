611e2a7695951796acff5b713e435b1a1a0d8beb
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2021-10-10 19:24:26 +0800

    meta tag

--- resources/views/layouts/m-layout-v2.blade.php
+++ resources/views/layouts/m-layout-v2.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="GenBI SULTRA">
+    <meta name="keywords" content="GenBI SULTRA, genbi sulawesi tenggara, GenBI">
+
+    <meta name="author" content="alfianizzah, fahrul">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GenBI Sultra</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
