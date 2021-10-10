5852928592600a08c68a0fbd57e34dabbb9c1942
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2021-10-10 19:02:46 +0800

    19.02

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -152,6 +152,29 @@
         }
 
     </style>
+    <script>
+        $(document).ready(function() {
+            $('.owl-carousel').owlCarousel({
+                // items: 2,
+                responsive: {
+                    0: {
+                        items: 1
+                        , nav: true
+                    }
+                    , 600: {
+                        items: 2
+                        , nav: false
+                    }
+                    , 1000: {
+                        items: 3
+                        , nav: true
+                        , loop: false
+                    }
+                }
+            });
+        });
+
+    </script>
     <!-- heroes -->
     <div class="container-fluid heroes-parent" style="background-image: url({{asset("img/welcome/sikola-dilao.jpg")}}">
 
