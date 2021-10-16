5749f05eb7c42f3219fa723f2f16863430068f5c
alfianizzah <alfianizzah6@gmail.com>
2021-10-16 12:29:43 +0800

    update profile

--- resources/views/components/dashboard/topbar.blade.php
+++ resources/views/components/dashboard/topbar.blade.php
@@ -1,8 +1,8 @@
       @php
-      use App\Models\User;
-      $user = User::find(Auth::user()->id)->first();
+          use App\Models\User;
+          $user = User::find(Auth::user()->id)->first();
 
-      $img =$user->profile->foto_profile??null;
+          $img = $user->profile->foto_profile ?? null;
 
       @endphp
       <nav class="navbar navbar-expand navbar-light bg-white topbar static-top">
@@ -18,14 +18,17 @@
 
               <!-- Nav Item - Search Dropdown (Visible Only XS) -->
               <li class="nav-item dropdown no-arrow d-sm-none">
-                  <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
+                      aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-search fa-fw"></i>
                   </a>
                   <!-- Dropdown - Messages -->
-                  <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                  <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                      aria-labelledby="searchDropdown">
                       <form class="form-inline mr-auto w-100 navbar-search">
                           <div class="input-group">
-                              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                              <input type="text" class="form-control bg-light border-0 small"
+                                  placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                               <div class="input-group-append">
                                   <button class="btn btn-primary" type="button">
                                       <i class="fas fa-search fa-sm"></i>
@@ -42,17 +45,21 @@
 
               <!-- Nav Item - User Information -->
               <li class="nav-item dropdown no-arrow">
-                  <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{$user->nama?? Auth::user()->name??""}}</span>
+                  <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                      aria-haspopup="true" aria-expanded="false">
+                      <span
+                          class="mr-2 d-none d-lg-inline text-gray-600 small">{{ $user->nama ?? (Auth::user()->name ?? '') }}</span>
 
 
-                      <img class="img-profile rounded-circle" src="{{$img?  asset("storage/".$img):"https://images.pexels.com/photos/6919779/pexels-photo-6919779.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" }}">
+                      <img class="img-profile rounded-circle"
+                          src="{{ $img ? asset('storage/' . $img) : 'https://images.pexels.com/photos/6919779/pexels-photo-6919779.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' }}">
 
 
 
                   </a>
                   <!-- Dropdown - User Information -->
-                  <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                  <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                      aria-labelledby="userDropdown">
                       <a class="dropdown-item" href="#">
                           <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                           Profile
@@ -77,7 +84,7 @@
       </nav>
       <div class="bg-white mb-4 px-4 d-flex justify-content-between py-2">
           <div class="left">
-              <h5>{{$title_page??""}}</h5>
+              <h5>{{ $title_page ?? '' }}</h5>
           </div>
 
       </div>
