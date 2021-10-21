5daa31416cab9cd8e555fa3f699743995bd1a7d2
alfianizzah <alfianizzah6@gmail.com>
2021-10-21 14:33:23 +0800

    A

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-let rPicker = document.querySelector("#start_spiner");
+// let rPicker = document.querySelector("#start_spiner");
 
-rPicker.addEventListener("click", function () {
-    let search = document.querySelector("[name=search]").value;
-    console.log(search.replace(/\r?\n|\r/g, " ").split(','));
-});
+// rPicker.addEventListener("click", function () {
+//     let search = document.querySelector("[name=search]").value;
+//     console.log(search.replace(/\r?\n|\r/g, " ").split(','));
+// });
