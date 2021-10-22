55a33ccc56344f9c8ff429f4ad27a86b4572c869
alfianizzah <alfianizzah6@gmail.com>
2021-10-22 16:49:16 +0800

    Seo

--- app/Http/Controllers/RandomPickerController.php
+++ app/Http/Controllers/RandomPickerController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Http\Request;
 
 class RandomPickerController extends Controller
 {
     public function show()
     {
+        SEOMeta::setTitle("Aplikasi GenBI SULTRA");
+        OpenGraph::addProperty('type', 'APPLIKASI RANDOM PICKER');
         return view("page.fitur.random_picker.show");
     }
 }
