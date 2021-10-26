2f2f4d6d4e8586bee21ef21fa68d3f4e692a71e3
alfianizzah <alfianizzah6@gmail.com>
2021-10-26 22:05:13 +0800

    Ad

--- resources/views/layouts/m-layout-v2.blade.php
+++ resources/views/layouts/m-layout-v2.blade.php
@@ -33,6 +33,8 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6167588362026240"
+     crossorigin="anonymous"></script>
     <script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
     </script>
 </head>
