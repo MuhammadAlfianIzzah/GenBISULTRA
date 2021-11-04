14b84b524004230c4ba2cbedbd26c634d9deb40f
alfianizzah <alfianizzah6@gmail.com>
2021-11-04 09:46:08 +0800

    contain

--- resources/views/page/user/users.blade.php
+++ resources/views/page/user/users.blade.php
@@ -13,7 +13,7 @@
                 @foreach ($users as $user)
                     <div class="col-4">
                         <div class="card">
-                            <img style="max-height: 200px;object-fit: cover"
+                            <img style="max-height: 200px;object-fit: contain"
                                 src="{{ asset("storage/$user->foto_profile") }}" class="card-img-top" alt="...">
                             <div class="card-header text-center">
                                 <a href="{{ route('users-search', $user->nama) }}">
