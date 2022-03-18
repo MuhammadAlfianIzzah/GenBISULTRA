6b24e8b112a5fe66ff23e4c49f9dd24e15d16a84
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2022-03-18 13:38:31 +0800

    full blog

--- resources/views/page/posts/detail.blade.php
+++ resources/views/page/posts/detail.blade.php
@@ -54,7 +54,7 @@
     <main class="container">
         <div class="row g-5 justify-content-center">
             <div class="col-md-11 col-12 overflow-hidden">
-                <article class="blog-post">
+                <article class="blog-post ">
                     <div class="alert alert-primary" role="alert">
                         Genbi, <span class="text-danger">{{ $post->created_at }}</span> by <a
                             href="{{ route('users-search', [$post->user->name]) }}">{{ $post->user->name }}</a>
@@ -62,7 +62,7 @@
                     <img style="max-height: 300px;object-fit: cover;width: 100%;object-position: center"
                         src="{{ asset("storage/$post->thumbnail") }}" alt="" class="img-thumbnail">
                 </article>
-                <article class="blog-post content pb-4">
+                <article class="blog-post full content pb-4">
                     {!! $post->content !!}
                 </article>
                 <div class="row mb-2">
