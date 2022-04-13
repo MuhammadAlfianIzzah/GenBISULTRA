982812b475978d9cb1450de595e1a74d9fcb632b
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2022-04-13 19:30:58 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::middleware(["role:super", "auth", "verified"])->group(function () {
     Route::get("/genbi/komsat", [KomsatController::class, "show"])->name("show-komsat");
     Route::post("/genbi/komsat", [KomsatController::class, "store"])->name("post-komsat");
     Route::get("/genbi/komsat/edit/{komisat:nama}", [KomsatController::class, "edit"])->name("edit-komsat");
-    Route::patch("/genbi/komsat/edit/{komisat:nama}", [KomsatController::class, "update"])->name("edit-komsat");
+    Route::patch("/genbi/komsat/edit/{komisat:nama}", [KomsatController::class, "update"])->name("update-komsat");
     Route::delete("/genbi/komsat/{komisat:nama}", [KomsatController::class, "delete"])->name("delete-komsat");
 
     Route::get("/manage/users", [AdminController::class, "users"])->name("manage-user");
@@ -66,7 +66,7 @@ Route::middleware(['role:admin|super|ketua', "auth", 'verified'])->group(functio
     // Kegiatan
     Route::get("/kegiatan/manage/myPost", [ActivityController::class, "myPost"])->name("mypost-kegiatan");
     Route::get("/kegiatan/manage/create", [ActivityController::class, "create"])->name("create-kegiatan");
-    Route::post("/kegiatan/manage/create", [ActivityController::class, "store"])->name("create-kegiatan");
+    Route::post("/kegiatan/manage/create", [ActivityController::class, "store"])->name("post-kegiatan");
     Route::get("/kegiatan/edit/{activities:slug}", [ActivityController::class, "edit"])->name("edit-kegiatan");
     Route::put("/kegiatan/edit/{activities:slug}", [ActivityController::class, "update"])->name("update-kegiatan");
     Route::delete("/kegiatan/manage/delete/{activities:slug}", [ActivityController::class, "delete"])->name("delete-kegiatan");
@@ -76,21 +76,21 @@ Route::middleware(['role:admin|super|ketua', "auth", 'verified'])->group(functio
     Route::post("/devisi/manage/show", [DevisiController::class, "store"])->name("create-devisi");
     Route::delete("/devisi/manage/delete/{devisi:nama}", [DevisiController::class, "delete"])->name("delete-devisi");
     Route::get("/devisi/manage/edit/{devisi:nama}", [DevisiController::class, "edit"])->name("edit-devisi");
-    Route::patch("/devisi/manage/edit/{devisi:nama}", [DevisiController::class, "update"])->name("edit-devisi");
+    Route::patch("/devisi/manage/edit/{devisi:nama}", [DevisiController::class, "update"])->name("update-devisi");
     // close devisi
 
     // kegiatan jenis
     Route::get("/kegiatan/manage/jenis", [TypeActivityController::class, "show"])->name("show-jenis-kegiatan");
     Route::post("/kegiatan/manage/jenis", [TypeActivityController::class, "store"])->name("create-jenis-activity");
     Route::get("/kegiatan/manage/jenis/edit/{typeActivity:nama}", [TypeActivityController::class, "edit"])->name("edit-jenis-kegiatan");
-    Route::patch("/kegiatan/manage/jenis/edit/{typeActivity:nama}", [TypeActivityController::class, "update"])->name("edit-jenis-kegiatan");
+    Route::patch("/kegiatan/manage/jenis/edit/{typeActivity:nama}", [TypeActivityController::class, "update"])->name("update-jenis-kegiatan");
 
     Route::delete("/kegiatan/jenis/delete/{TypeActivity:id}", [TypeActivityController::class, "delete"])->name("delete-jenis-activity");
 
     // close Kegiatan
     // kategory post
     Route::get("/post/manage/category", [CategoryPostController::class, "show"])->name("category-posts");
-    Route::post("/post/manage/category", [CategoryPostController::class, "store"])->name("category-posts");
+    Route::post("/post/manage/category", [CategoryPostController::class, "store"])->name("post-category");
     Route::delete("/post/manage/jenis/delete/{categoryPosts:id}", [CategoryPostController::class, "delete"])->name("delete-category-posts");
     // end kategory post
 
@@ -111,7 +111,7 @@ Route::middleware(["auth"])->group(function () {
     Route::delete("/List-Mi-Cepat", [ListMiCepatController::class, "destroy"])->name("delete-listmi");
     Route::post("/List-Mi-Cepat", [ListMiCepatController::class, "store"]);
     Route::get("/List-Mi-Cepat/response/{id}", [ListMiCepatController::class, "response"])->name("respon-list");
-    Route::post("/List-Mi-Cepat/response/{id}", [ListMiCepatController::class, "storeresponse"])->name("respon-list");
+    Route::post("/List-Mi-Cepat/response/{id}", [ListMiCepatController::class, "storeresponse"])->name("post-list");
     Route::delete("/List-Mi-Cepat/response/{id}", [ListMiCepatController::class, "destroyresponse"])->name("delete-respon-list");
     Route::get("/List-Mi-Cepat/response/{id}/export", [ListMiCepatController::class, "export"])->name("export-reponListmi");
 });
@@ -119,14 +119,14 @@ Route::middleware(["auth"])->group(function () {
 Route::middleware(['auth', "verified"])->group(function () {
     // genbi
     Route::get("/genbi/daftar", [GenbiController::class, "create"])->name("daftar-genbi");
-    Route::post("/genbi/daftar", [GenbiController::class, "store"])->name("daftar-genbi");
+    Route::post("/genbi/daftar", [GenbiController::class, "store"])->name("post-genbi");
     Route::get("/genbi/gabung/departement", [GenbiController::class, "gDepartement"])->name("gabung-departement");
 
     // genbi
 
     // brain post
     Route::get("/my-brain/manage/create", [MyBrainController::class, "create"])->name("write-brain");
-    Route::post("/my-brain/manage/create", [MyBrainController::class, "store"])->name("write-brain");
+    Route::post("/my-brain/manage/create", [MyBrainController::class, "store"])->name("post-brain");
     Route::get("/my-brain/edit/{brain_post:slug}", [MyBrainController::class, "edit"])->name("edit-brain");
     Route::put("/my-brain/edit/{brain_post:slug}", [MyBrainController::class, "update"])->name("update-brain");
     Route::get("/my-brain/manage/my-post", [MyBrainController::class, "showAll"])->name("my-brainPost");
@@ -137,7 +137,7 @@ Route::middleware(['auth', "verified"])->group(function () {
 
     // posts
     Route::get("/post/manage/create", [PostsController::class, "create"])->name("create-posts");
-    Route::post("/post/manage/create", [PostsController::class, "store"])->name("create-posts");
+    Route::post("/post/manage/create", [PostsController::class, "store"])->name("post-posts");
 
     Route::get("/post/manage/my-post", [PostsController::class, "myPosts"])->name("my-posts");
     Route::delete("/post/manage/delete/{posts:slug}", [PostsController::class, "delete"])->name("delete-posts");
@@ -156,7 +156,7 @@ Route::middleware(['auth', "verified"])->group(function () {
     Route::get("/user/profile/lengkapi-data", function () {
         return view("page.user.set-profile");
     })->name("set-profile");
-    Route::post("/user/profile/lengkapi-data", [ProfileController::class, "store"])->name("set-profile");
+    Route::post("/user/profile/lengkapi-data", [ProfileController::class, "store"])->name("post-set-profile");
     // close profile
 });
 // Route::get('users/export', [UserController::class, "export"])    ;
@@ -177,7 +177,7 @@ Route::get("/tentangKami/info", function () {
 })->name("tentang-kami");
 Route::get("/post/{posts:slug}", [PostsController::class, "detail"])->name("detail-posts");
 Route::get("/qrcode/generate", [QrcodeController::class, "show"])->name("show-qrcode");
-Route::post("/qrcode/generate", [QrcodeController::class, "generate"])->name("show-qrcode");
+Route::post("/qrcode/generate", [QrcodeController::class, "generate"])->name("post-qrcode");
 
 Route::get("/random-picker", [RandomPickerController::class, "show"])->name("random-picker");
 
